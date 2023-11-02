21076bcdf6443fa428c35bf73855a620ac4fc3ce
Arjay-boop <117987767+Arjay-boop@users.noreply.github.com>
2023-11-02 15:06:32 +0800

    Register

--- register.php
+++ register.php
@@ -3,47 +3,74 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/login_register.css">
+    <link rel="icon" type="image/x-icon" href="img/RottenPopCorn(LogoOnly).png">
+    <script src="tailwind.js"></script>
     <title>Register</title>
 </head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Gabarito:wght@400;500;600;700;800;900&display=swap');
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        width: 100vw;
+        overflow-x: hidden;
+        background-image: url('img/bg-image.jpg');
+        background-size: cover;
+        font-family: 'Gabarito', sans-serif;
+    }
+</style>
 <body>
-    <div class="main">
-        <div class="sub-register">
-            <div class="register">
-                <div class="picture">
-                    <img class="pic" src="img/RottenPopCorn(LogoOnly).png" alt="">
-                </div>
-                <form class="register" action="">
-                    <div class="full-name">
-                        <label for="">Full Name</label>
-                        <input type="text" name="" id="" placeholder="Full Name">
+  <div class="flex flex-col min-h-screen items-center justify-center">
+      <div class="w-1/2 bg-[#000000aa] py-10 px-20 rounded-lg shadow">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="w-full text-3xl font-bold text-white border-b-2 pb-4">
+                  Create and account
+              </h1>
+              <form class="space-y-2" action="#">
+                    <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                      <input autocomplete="off" type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Username" required="">
                     </div>
-                    <div class="sex">
-                        <label for="">Sex</label>
-                        <input type="text" name="" id="" placeholder="Sex">
+                    <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Birthday</label>
+                      <input autocomplete="off" type="date" name="birthday" id="birthday" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="">
+                  </div>
+                    <div>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <input autocomplete="off" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Email" required="">
                     </div>
-                    <div class="address">
-                        <label for="">Address</label>
-                        <input type="text" name="" id="" placeholder="Address">
+                    <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <input autocomplete="off" type="password" name="password" id="password" placeholder="Enter Password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                     </div>
-                    <div class="email">
-                        <label for="">Email</label>
-                        <input type="email" name="" id="" placeholder="Email">
+                    <div>
+                        <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                        <input autocomplete="off" type="confirm-password" name="confirm-password" id="confirm-password" placeholder="Confirm Password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                     </div>
-                    <div class="password">
-                        <label for="">Password</label>
-                        <input type="password" name="" id="" placeholder="Password">
+                    <div class="flex items-start">
                     </div>
-                    <div class="con-password">
-                        <label for="">Confirm Password</label>
-                        <input type="password" name="" id="" placeholder="Confirm Password">
-                    </div>
-                    <div class="button">
-                        <input type="submit" name="" id="" class="submit" value="Register">
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+                  <button type="submit" class="w-full text-white bg-red-900 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Already have an account? <a href="login.php" class="font-medium text-red-600 hover:underline dark:text-primary-500">Login here</a>
+                  </p>
+              </form>
+          </div>
+      </div>
+  </div>
 </body>
+
+<script>
+    window.addEventListener('blur', () => {
+        document.title = "I miss you comeback 💔 - RottenPopcorn";
+    })
+
+    window.addEventListener('focus', () => {
+        document.title = "Rotten Popcorn";
+    })
+</script>
 </html>

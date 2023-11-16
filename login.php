004c86cc6d1eaa429db5f71a6e37416c441c5da3
Aaron Jay N. Gabato <aaronjaygabato30@gmail.com>
2023-11-16 09:12:57 +0800

    add login button for facebookand google

--- login.php
+++ login.php
@@ -22,7 +22,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
             session_start();
 
             $_SESSION['userID'] = $result[0]['userID'];
-            header("Location: index.php");
+            if ($result[0]['role'] == "Admin") {
+                header("Location: admin_content.php");
+            } else {
+                header("Location: index.php");
+            }
             exit();
         } else {
             echo '<script>alert("Invalid email or password.");</script>';
@@ -64,6 +68,76 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
         background-image: url('img/bg-image.jpg');
         background-size: cover;
         font-family: 'Gabarito', sans-serif;
+    }
+
+    .loginBtn {
+        box-sizing: border-box;
+        position: relative;
+        /* width: 13em;  - apply for fixed size */
+        margin: 0.2em;
+        padding: 0 15px 0 46px;
+        border: none;
+        text-align: left;
+        line-height: 34px;
+        white-space: nowrap;
+        border-radius: 0.2em;
+        font-size: 16px;
+        color: #FFF;
+    }
+
+    .loginBtn:before {
+        content: "";
+        box-sizing: border-box;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 34px;
+        height: 100%;
+    }
+
+    .loginBtn:focus {
+        outline: none;
+    }
+
+    .loginBtn:active {
+        box-shadow: inset 0 0 0 32px rgba(0, 0, 0, 0.1);
+    }
+
+
+    /* Facebook */
+    .loginBtn--facebook {
+        background-color: #4C69BA;
+        background-image: linear-gradient(#4C69BA, #3B55A0);
+        /*font-family: "Helvetica neue", Helvetica Neue, Helvetica, Arial, sans-serif;*/
+        text-shadow: 0 -1px 0 #354C8C;
+    }
+
+    .loginBtn--facebook:before {
+        border-right: #364e92 1px solid;
+        background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_facebook.png') 6px 6px no-repeat;
+    }
+
+    .loginBtn--facebook:hover,
+    .loginBtn--facebook:focus {
+        background-color: #5B7BD5;
+        background-image: linear-gradient(#5B7BD5, #4864B1);
+    }
+
+
+    /* Google */
+    .loginBtn--google {
+        /*font-family: "Roboto", Roboto, arial, sans-serif;*/
+        background: #DD4B39;
+    }
+
+    .loginBtn--google:before {
+        border-right: #BB3F30 1px solid;
+        background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_google.png') 6px 6px no-repeat;
+    }
+
+    .loginBtn--google:hover,
+    .loginBtn--google:focus {
+        background: #E74B37;
     }
 </style>
 
@@ -110,6 +184,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
                     <div class="border-t flex-1 mx-4"></div>
                     <p class="mx-4 mb-0 text-center font-medium dark:text-white">Or</p>
                     <div class="border-t flex-1 mx-4"></div>
+                </div>
+
+                <div class="flex flex-row gap-x-4">
+                    <button class="loginBtn loginBtn--facebook">
+                        Login with Facebook
+                    </button>
+
+                    <button class="loginBtn loginBtn--google">
+                        Login with Google
+                    </button>
                 </div>
 
                 <h2 class="text-center text-white text-xl hover:text-red-700 hover:underline duration-200"><a href="register.php">Doesn't have an account?</a></h2>
